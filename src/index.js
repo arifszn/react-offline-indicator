e1a1a219e41d57365ac37d875cbfe3c9b561c066
Ariful Alam <ariful.alam@monstar-lab.com>
2022-08-16 17:14:56 +0600

    Add tailwind css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import './css/index.css';
+
+const ReactOfflineStatus = (props) => {
+  return '';
+};
+
+ReactOfflineStatus.propTypes = {};
+
+export default ReactOfflineStatus;
