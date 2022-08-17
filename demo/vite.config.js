e0d3fb7ea8013892db1fc5a6a239cdf0bc40c795
Ariful Alam <swazan.arif@gmail.com>
2022-08-18 00:33:32 +0600

    Set custom server config

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-offline-detect/',
   plugins: [react()],
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), '..'],
+    },
+  },
 });
